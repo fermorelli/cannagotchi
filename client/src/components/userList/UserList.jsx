4bf4c8b6da4c61b77acedf459cbc8b3ce160cffb
fermorelli <fer.cmorelli@gmail.com>
2022-10-28 13:16:28 -0300

    delete method first attempt

--- client/src/components/userList/UserList.jsx
+++ client/src/components/userList/UserList.jsx
@@ -3,10 +3,13 @@ import { useState } from "react";
 import './userlist.css';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { GoTrashcan } from 'react-icons/go'
+import { Link } from "react-router-dom";
+import { Modal } from '../modal/modal';
 
 export const UserList = ()=> {
 
     const [ users, setUsers ] = useState([]);
+    const [status, setStatus] = useState();
 
     const getUsers = async () =>{
         const response = await fetch('http://localhost:8080/users');
@@ -20,18 +23,25 @@ export const UserList = ()=> {
         getUsers();
     },[])
 
+    const deleteUser = (id)=>{
+        console.log('id:',id)
+}
+
     return (
         <div id="#home" className="all">
             <h2 className="title">Users List</h2>
-            <div className="userList">
+            <div className="userList"> 
+                <Modal></Modal>
                 {users.map((user)=>{
                     return(
                         <div className="userCard">
                             <div className="header">
                                 <h3>{user.firstName} {user.lastName}</h3>
                                 <div className="headerButtons">
-                                    <BsFillPencilFill />
-                                    <GoTrashcan />
+                                    <Link to={'/edit-user'}>
+                                        <BsFillPencilFill />
+                                    </Link>
+                                    <GoTrashcan onClick={deleteUser(user._id)}/>
                                 </div>
                             </div>
                             <span>{user.email}</span>
