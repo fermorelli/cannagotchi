d29dd344c2c3986b14fe682ac6f96513188f04e6
ilcosme <fer.cmorelli@gmail.com>
2022-10-29 15:16:26 -0300

    delete method working

--- client/src/components/userList/UserList.jsx
+++ client/src/components/userList/UserList.jsx
@@ -9,7 +9,7 @@ import { Modal } from '../modal/modal';
 export const UserList = ()=> {
 
     const [ users, setUsers ] = useState([]);
-    const [status, setStatus] = useState();
+    const [ isOpen, setIsOpen ] = useState(false);
 
     const getUsers = async () =>{
         const response = await fetch('http://localhost:8080/users');
@@ -17,31 +17,44 @@ export const UserList = ()=> {
         setUsers(data.data);
     }
 
-    console.log(users);
-
     useEffect(()=>{
         getUsers();
     },[])
 
     const deleteUser = (id)=>{
+        fetch(`http://localhost:8080/${id}`, {method: 'DELETE'})
+        .then((response)=> {
+            if(!response.ok){
+                throw new Error('Something went wrong')
+            }else{
+                setIsOpen(true)
+                setUsers(users.filter(item=>item._id !== id))
+            }
+        }).catch((e)=>console.log(e))
+
         console.log('id:',id)
 }
 
     return (
         <div id="#home" className="all">
             <h2 className="title">Users List</h2>
-            <div className="userList"> 
-                <Modal></Modal>
+            <div className="userList">
+            {isOpen &&
+                <Modal setIsOpen={setIsOpen} modalTitle={'User successfully deleted'}>
+                    <Link to={'/'}>
+                        <button onClick={()=>setIsOpen(false)}>Go back</button>
+                    </Link>
+                </Modal>}
                 {users.map((user)=>{
                     return(
-                        <div className="userCard">
+                        <div className="userCard" key={user._id}>
                             <div className="header">
                                 <h3>{user.firstName} {user.lastName}</h3>
                                 <div className="headerButtons">
                                     <Link to={'/edit-user'}>
                                         <BsFillPencilFill />
                                     </Link>
-                                    <GoTrashcan onClick={deleteUser(user._id)}/>
+                                    <GoTrashcan onClick={()=>deleteUser(user._id)}/>
                                 </div>
                             </div>
                             <span>{user.email}</span>
