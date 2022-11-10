bd21d62bb649843d7a0952b57f57254732723991
ilcosme <fer.cmorelli@gmail.com>
2022-11-10 16:33:53 -0300

    firebase login setup

--- client/src/components/login/LogIn.jsx
+++ client/src/components/login/LogIn.jsx
@@ -3,14 +3,24 @@ import { appendErrors, useForm } from 'react-hook-form';
 import { schema } from './validation';
 import { joiResolver } from '@hookform/resolvers/joi';
 import { useState } from 'react';
+import { useAuth } from '../../context/authContext';
+import { useNavigate } from 'react-router-dom';
 
 export const LogIn = ()=>{
 
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
+    const navigate = useNavigate();
+    const { login } = useAuth();
 
-    const logIn = ()=>{
-
+    const logIn = async (e)=>{
+        e.preventDefault();
+        try {
+            await login(email,password);
+            navigate('/users')
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     const { register, handleSubmit, formState: { errors } } = useForm({
--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,5 +1,5 @@
-import { createContext, useContext } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase/firebase';
 
 export const authContext = createContext();
@@ -11,13 +11,28 @@ export const useAuth = () =>{
 
 export const AuthProvider = ({children})=>{
 
+    const [ user, setUser ] = useState();
+
     const regNew = (email, password) => {
         createUserWithEmailAndPassword(auth, email, password);
         console.log('created');
     }
 
+    const login = (email,password) => {
+        signInWithEmailAndPassword(auth, email, password);
+        console.log('access ok');
+    }
+
+    useEffect(()=>{
+        console.log('cargo');
+        onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('asd: ', user);
+        });
+    },[])
+
     return(
-        <authContext.Provider value={{regNew}}>
+        <authContext.Provider value={{regNew, login}}>
             {children}
         </authContext.Provider>
     )
